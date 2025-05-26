90f2c1ed039af4e37c542b61903ebb999ccd1541
EngFAlAttar <engfalattar@gmail.com>
2025-05-26 10:31:32 +0300

    Feature: create/display products

---
+++ ProductDashboard/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProductDashboard.Data;
+using ProductDashboard.Models;
+
+namespace ProductDashboard.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private AppDbContext db;
+
+        public ProductsController(AppDbContext _db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.Products.Include(p => p.Category));
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.allCategories = new SelectList(db.Categories, "CategoryId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
